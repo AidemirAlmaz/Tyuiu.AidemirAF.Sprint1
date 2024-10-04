e1df75c7e001e99c33ea0cada8e636dd5b3cc6ec
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-10-04 22:58:28 +0500

    Task4

---
+++ Tyuiu.AidemirAF.Sprint1.Task4.V16.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.AidemirAF.Sprint1.Task4.V16.Lib
+{
+    public class DataService : ISprint1Task4V16
+    {
+        public double Calculate(double x)
+        {
+            double res = Math.Round(1 / (x + 4), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.AidemirAF.Sprint1.Task4.V16/Program.cs
@@ -0,0 +1,37 @@
+﻿//ЗАДАНИЕ
+//Написать программу, которая запрашивает у пользователя исходные данные,
+//вычисляет результат по формуле и печатает его на экране.
+//Формула:
+//
+//   -------
+//   1/(x+4)
+
+using Tyuiu.AidemirAF.Sprint1.Task4.V16.Lib;
+
+namespace Tyuiu.AidemirAF.Sprint1.Task4.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                  *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            double x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                        *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            Console.WriteLine("1/(x+4) = " + ds.Calculate(x));
+            Console.ReadLine();
+
+        }
+    }
+}
