c37a6dba4f523076bf98586e94f0e4899273c4f8
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-10-04 20:00:22 +0500

    Add project files.

---
+++ Tyuiu.AidemirAF.Sprint1.Task0.V13.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿//Важно! Следует к библиотеке классов подключить файл tyuiu.cources.programming.interfaces.dll
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.AidemirAF.Sprint1.Task0.V13.Lib
+{
+    public class DataService : ISprint1Task0V13
+    {
+        public double Calculate()
+        {
+            return 24 / (6 * 2) - 24 / 6 / 4;
+        }
+    }
+}
